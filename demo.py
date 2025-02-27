de5c49cfa635c17044b475e8b7f9d6e189f6adae
Fabian Joos <bubenlouis@gmail.com>
2025-02-27 13:21:01 +0100

    Rework player initialization in demo.py by using add_players method Add list of player objects Add single player object

--- demo.py
+++ demo.py
@@ -7,20 +7,23 @@ end_date = datetime(2025,5,30)
 
 vp = CfTournament(start_date, end_date)
 
-vp.players = [CfPlayer("John", "Apple", 1450),
-              CfPlayer("Jane", "Flowers", 1750),
-              CfPlayer("Michael", "Hunter", 1200),
-              CfPlayer("Emily", "Smith", 1600),
-              CfPlayer("David", "Porter", 1300),
-              CfPlayer("Sarah", "Waters", 1900),
-              CfPlayer("Robert", "Peters", 1550),
-              CfPlayer("Laura", "Howard", 1400)]
+player_list = [CfPlayer("John", "Apple", 1450),
+               CfPlayer("Jane", "Flowers", 1750),
+               CfPlayer("Michael", "Hunter", 1200),
+               CfPlayer("Emily", "Smith", 1600),
+               CfPlayer("David", "Porter", 1300),
+               CfPlayer("Sarah", "Waters", 1900),
+               CfPlayer("Robert", "Peters", 1550)]
+
+single_player = CfPlayer("Laura", "Howard", 1400)
+
+vp.add_players(player_list)
+vp.add_players(single_player)
 
 vp.round_robin_pairing()
+vp.match_schedule()
 
 vp.matchdays[0][0].black_wins()
 vp.matchdays[0][1].white_wins()
 vp.matchdays[0][2].draw()
 vp.matchdays[0][3].white_wins()
-
-vp.match_schedule()
